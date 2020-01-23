3412bb411726c0e584899ae070ba85a18d25f13c
Anton <kaban.studio@gmail.com>
2020-01-23 21:54:45 +0200

    two

--- kalkulator.js
+++ kalkulator.js
@@ -1,18 +1,23 @@
-	for (var i=0; i<radioPaint.length; i++) {
-		radioPaint[i].onchange = testRadioPaint;
+	
+	
+function  butoonStart () {
 
+	var radioPaintJS = document.getElementsByName ('radioButton');
+	for (var i=0; i<radioPaint.length; i++) {
+		radioPaintJS[i].onchange = testRadioPaint;
 		function testRadioPaint () {
 			var paintCoef = this.value
-			console.log (paintCoef);}
+			console.log (paintCoef);
 		}
+	}
 
-	var radioConstr = document.getElementsByName ('radioButtonConstr');
 
+	var radioConstrJS = document.getElementsByName ('radioButtonConstr');
 	for (var i=0; i<radioConstr.length; i++) {
-		radioConstr[i].onchange = testRadioConstr;
+		radioConstrJS[i].onchange = testRadioConstr;
 		function testRadioConstr () {
 			var constrCoef = this.value
-			console.log (constrCoef);}
-		
+			console.log (constrCoef);
+		}
 	}
-asdad
+}
