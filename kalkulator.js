f97b2c622f9e60e8d9755196dd82030bff2c9c57
Anton <kaban.studio@gmail.com>
2020-01-23 22:11:46 +0200

    three

--- kalkulator.js
+++ kalkulator.js
@@ -3,21 +3,20 @@
 function  butoonStart () {
 
 	var radioPaintJS = document.getElementsByName ('radioButton');
-	for (var i=0; i<radioPaint.length; i++) {
+	for (var i=0; i<radioPaintJS.length; i++) {
 		radioPaintJS[i].onchange = testRadioPaint;
+	}
 		function testRadioPaint () {
 			var paintCoef = this.value
 			console.log (paintCoef);
 		}
-	}
 
 
 	var radioConstrJS = document.getElementsByName ('radioButtonConstr');
-	for (var i=0; i<radioConstr.length; i++) {
+	for (var i=0; i<radioConstrJS.length; i++) {
 		radioConstrJS[i].onchange = testRadioConstr;
+	}
 		function testRadioConstr () {
 			var constrCoef = this.value
 			console.log (constrCoef);
-		}
-	}
-}
+		}}
